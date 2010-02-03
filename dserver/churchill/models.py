757648e3c30952dcca1d4dc77bed6b7b966ab081
Will Colton <coltonw@gmail.com>
2010-02-02 20:55:01 -0500

    Added a field to the Player model

--- dserver/churchill/models.py
+++ dserver/churchill/models.py
@@ -20,6 +20,8 @@ class Player(models.Model):
     number = models.IntegerField() #This is the order the players joined the game. 
     gold = models.IntegerField(default=0)
     character = models.IntegerField(default=-1) #characters are 0-7 their order of play.
+    turn_step = models.IntegerField(default=-1) #-1= not my turn, 0= start of turn, 1= taking action, 2=building district, 3= end of turn
+    used_character_ability = models.BooleanField(default=False) 
     hand = models.CommaSeparatedIntegerField(max_length=100,default="")
     played = models.CommaSeparatedIntegerField(max_length=100,default="")
     
